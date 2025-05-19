1439fdfd480b6b025bc6a331a066da2e7f7ebee8
mohdsaadshaikh <mohammadsaad925s4s@gmail.com>
2025-05-19 07:00:48 -0700

    configured layout with appshell

--- src/renderer/src/components/Logo.tsx
+++ src/renderer/src/components/Logo.tsx
@@ -3,7 +3,17 @@ import React from 'react'
 
 const Logo = (): React.JSX.Element => {
   return (
-    <Text fz="h1" style={{ fontFamily: 'Audiowide' }}>
+    <Text
+      fz="h1"
+      fw={900}
+      style={{
+        fontFamily: 'Audiowide',
+        background: 'linear-gradient(90deg, #00ffff, #00ff88)',
+        WebkitBackgroundClip: 'text',
+        WebkitTextFillColor: 'transparent'
+        // textShadow: '0 0 5px #0ff, 0 0 10px #f0f, 0 0 15px #0ff'
+      }}
+    >
       CANIGAME
     </Text>
   )
--- src/renderer/src/layout/index.tsx
+++ src/renderer/src/layout/index.tsx
@@ -1,9 +1,37 @@
 import Logo from '@renderer/components/Logo'
+import { AppShell, Burger, Flex } from '@mantine/core'
+import { useDisclosure } from '@mantine/hooks'
 
+export function Demo(): React.JSX.Element {
+  const [opened, { toggle }] = useDisclosure()
+
+  return (
+    <AppShell
+      header={{ height: 75 }}
+      navbar={{
+        width: 250,
+        breakpoint: 'sm',
+        collapsed: { mobile: !opened }
+      }}
+      padding="md"
+    >
+      <AppShell.Header>
+        <Flex align="center" h="100%" px="md">
+          <Burger opened={opened} onClick={toggle} hiddenFrom="sm" size="sm" />
+          <Logo />
+        </Flex>
+      </AppShell.Header>
+
+      <AppShell.Navbar p="md"></AppShell.Navbar>
+
+      <AppShell.Main>Main</AppShell.Main>
+    </AppShell>
+  )
+}
 const AppLayout = (): React.JSX.Element => {
   return (
     <div>
-      <Logo />
+      <Demo />
     </div>
   )
 }
