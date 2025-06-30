6d4d2895f27051e14f2410e800dc1ae0956177f9
mohdsaadshaikh <mohammadsaad925s4s@gmail.com>
2025-06-30 12:12:06 +0500

    Add type for Hardware information

---
+++ src/types/index.ts
@@ -0,0 +1,46 @@
+export interface CPUInfo {
+  brand: string
+  manufacturer: string
+  model: string
+  cores: number
+  physicalCores: number
+  processors: number
+  speed: number
+  speedMax: number
+  speedMin: number
+}
+
+export interface MemoryInfo {
+  total: number
+  free: number
+  used: number
+  active: number
+  available: number
+}
+
+export interface GPUInfo {
+  vendor: string
+  model: string
+
+  vram: number
+}
+
+export interface OSInfo {
+  platform: string
+  hostname: string
+}
+
+export interface SystemInfo {
+  manufacturer: string
+  model: string
+  version: string
+}
+
+export interface HardwareInfo {
+  cpu: CPUInfo
+  memory: MemoryInfo
+  gpu: GPUInfo[]
+  os: OSInfo
+  system: SystemInfo
+  timestamp: number
+}
